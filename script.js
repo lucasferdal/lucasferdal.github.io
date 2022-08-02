8e15c7d8f54c9840f6b04c40c54ed486bf64e4e2
lucasferdal <100643517+lucasferdal@users.noreply.github.com>
2022-08-02 12:19:06 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let imagen = document.getElementsByTagName('body');
+
+console.log('hola')
+function asd() {
+    let acordion = document.getElementsByTagName('button')
+acordion.addEventListener('click', porElAmorDeDiosFuncionaDeUnaVez())
+}
+
+function porElAmorDeDiosFuncionaDeUnaVez() {
+    return(
+    console.log('hola'))
+}
